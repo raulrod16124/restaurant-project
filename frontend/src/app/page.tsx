2c3ab41567e4e79bbdaacb52338aa30a8a852718
raul rodriguez <raul.rod16124@gmail.com>
2024-11-08 16:23:37 +0000

    Add tables and menu links

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
         Home
+        <div>
+          <Link href="/tables">Tables</Link>
+          <Link href="/menu">Menu</Link>
+        </div>
     </div>
   );
 }
