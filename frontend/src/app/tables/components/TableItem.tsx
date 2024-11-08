6eed4251b5febdd998be822e9828a12c50bf5f52
raul rodriguez <raul.rod16124@gmail.com>
2024-11-08 00:27:08 +0000

    add table style and table state text

--- frontend/src/app/tables/components/TableItem.tsx
+++ frontend/src/app/tables/components/TableItem.tsx
@@ -1,18 +1,36 @@
+import "./../tables.css"
 import React from 'react'
 import emptyTable from "@/assets/empty_table.png"
 import waitingTable from "@/assets/waiting_table.png"
 import attendedTable from "@/assets/attended_table.png"
 import { Table } from '@/types'
+import Image, { StaticImageData } from 'next/image'
 
 interface IProps {
   table: Table
 }
 
 export const TableItem = ({table}: IProps) => {
+  const tableImage: Record<string, StaticImageData> = {
+    empty: emptyTable,
+    waiting: waitingTable,
+    attended: attendedTable
+  }
+  const tableText: Record<string, string> = {
+    empty: "Mesa libre",
+    waiting: "Mesa ocupada",
+    attended: "Mesa atendida"
+  }
+
   return (
-    <div>
-      <p>{table.id}</p>
-      <p>{table.state}</p>
+    <div className="table">
+      <Image 
+        src={tableImage[table.state.toLowerCase()]} 
+        alt='table-image'
+        width={180}
+        height={180}
+      />
+      <p>{tableText[table.state.toLowerCase()]}</p>
     </div>
   )
 }
