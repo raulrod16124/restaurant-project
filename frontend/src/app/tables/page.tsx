0c6bb8fa4689406e4af47b965a7223f5f93a0d4a
raul rodriguez <raul.rod16124@gmail.com>
2024-11-11 00:01:46 +0000

    remove unused import

--- frontend/src/app/tables/page.tsx
+++ frontend/src/app/tables/page.tsx
@@ -3,7 +3,6 @@ import styles from "./tables.module.css"
 import { Table } from "@/types";
 import { useEffect, useState } from "react";
 import { TableItem } from "./components/TableItem";
-import Link from "next/link";
 import { getTablesAction } from "@/services/tables";
 
 export default function Tables() {
